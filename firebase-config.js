a315d7aa7582c8ac3960dd6d3da48ab8298240b9
Ayse Erduran <ayseerduran@Ayses-MBP.lan>
2023-04-23 18:17:43 -0400

    updates

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyC4ji6O24VdCF_b5kJqDga0e2kAAc27pCM";
+export const FIREBASE_AUTH_DOMAIN = "leros-humanism-project.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://leros-humanism-project-default-rtdb.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "leros-humanism-project";
+export const FIREBASE_STORAGE_BUCKET = "leros-humanism-project.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "1093565678690";
+export const FIREBASE_APP_ID = "1:1093565678690:web:b4f4b98a70845c853fbd3b";
+export const FIREBASE_MEASUREMENT_ID = "G-59EM4P1LL3";
